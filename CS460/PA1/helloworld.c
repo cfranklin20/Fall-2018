03c5cfd872139dfd0637f8907b621196f4cf7fb6
Clayton Franklin <clayton.j.franklin@nevada.unr.edu>
2018-08-27 19:35:05 -0700

    Adding the Hello World file to the PA1 Folder

---
+++ CS460/PA1/helloworld.c
@@ -0,0 +1,12 @@
+/* helloworld.c
+   Author: Clayton Franklin
+   Date: Monday August 27, 2018
+   Class: CS 460
+This is the first problem for PA1
+*/
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello World");
+}
